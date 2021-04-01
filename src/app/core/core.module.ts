ab8f0ecbbe58a051fbbefd05af3dc25ffe40f441
azizul hoq <azizulmaruf@gmail.com>
2021-04-02 00:00:05 +0600

    core-structure - added core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './services/api-service.service';
+import { StorageService } from './services/storage-service.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  providers: []
+})
+export class CoreModule { }
