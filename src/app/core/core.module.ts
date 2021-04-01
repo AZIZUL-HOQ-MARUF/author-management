032e0edc0918a07ef882dab452297a940a55536f
azizul hoq <azizulmaruf@gmail.com>
2021-04-02 00:37:49 +0600

    core-structure - singleton core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from './services/api-service.service';
 import { StorageService } from './services/storage-service.service';
@@ -10,4 +10,10 @@ import { StorageService } from './services/storage-service.service';
   declarations: [],
   providers: [ApiService, StorageService]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('Core Module Has Already been Loaded, Import Core Module only in AppModule');
+    }
+  }
+ }
